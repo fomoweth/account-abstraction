84420785dde8166ea6048534623680bbb02bf855
Ryan <rkim@greenwood.app>
2025-02-24 18:23:59 -0800

    added validations for updating safe senders

--- src/modules/validators/K1Validator.sol
+++ src/modules/validators/K1Validator.sol
@@ -8,7 +8,7 @@ import {BytesLib} from "src/libraries/BytesLib.sol";
 import {EnumerableSet4337} from "src/libraries/EnumerableSet4337.sol";
 import {ValidationData} from "src/types/Types.sol";
 import {ERC1271} from "src/utils/ERC1271.sol";
-import {HybridValidatorBase} from "./HybridValidatorBase.sol";
+import {HybridValidatorBase} from "../base/HybridValidatorBase.sol";
 
 /// @title K1Validator
 /// @notice Verifies user operation signatures for smart accounts
@@ -20,22 +20,25 @@ contract K1Validator is IK1Validator, HybridValidatorBase, ERC1271 {
 	using SignatureCheckerLib for address;
 	using SignatureCheckerLib for bytes32;
 
-	mapping(address account => address owner) internal _accountOwners;
+	/// @dev keccak256(abi.encode(uint256(keccak256("eip7579.module.accountOwners")) - 1)) & ~bytes32(uint256(0xff))
+	bytes32 internal constant ACCOUNT_OWNERS_SLOT = 0x0bbe1ebe3453361add0e00d1239aa291b315d54a2a4ce1b8c20d3c415bb3e300;
 
 	EnumerableSet4337.AddressSet private _safeSenders;
 
 	function onInstall(bytes calldata data) external payable {
 		require(!_isInitialized(msg.sender), AlreadyInitialized(msg.sender));
-
 		_setAccountOwner(_checkAccountOwner(data.toAddress(0)));
 		if (data.length > 96) _fillSafeSenders(data.toAddressArray(1));
 	}
 
 	function onUninstall(bytes calldata) external payable {
 		require(_isInitialized(msg.sender), NotInitialized(msg.sender));
-
-		_safeSenders.removeAll(msg.sender);
 		_setAccountOwner(address(0));
+		_safeSenders.removeAll(msg.sender);
+	}
+
+	function isInitialized(address account) external view returns (bool) {
+		return _isInitialized(account);
 	}
 
 	function transferOwnership(address owner) external {
@@ -48,10 +51,12 @@ contract K1Validator is IK1Validator, HybridValidatorBase, ERC1271 {
 	}
 
 	function addSafeSender(address sender) external {
+		require(_isInitialized(msg.sender), NotInitialized(msg.sender));
 		_addSafeSender(sender);
 	}
 
 	function removeSafeSender(address sender) external {
+		require(_isInitialized(msg.sender), NotInitialized(msg.sender));
 		require(_safeSenders.remove(msg.sender, sender), SenderNotExists(sender));
 	}
 
@@ -90,15 +95,15 @@ contract K1Validator is IK1Validator, HybridValidatorBase, ERC1271 {
 		return _validateSignatureForOwner(data.toAddress(), hash, signature);
 	}
 
-	function name() external pure virtual override returns (string memory) {
+	function name() external pure returns (string memory) {
 		return "K1Validator";
 	}
 
-	function version() external pure virtual override returns (string memory) {
+	function version() external pure returns (string memory) {
 		return "1.0.0";
 	}
 
-	function _isInitialized(address account) internal view virtual override returns (bool) {
+	function _isInitialized(address account) internal view returns (bool) {
 		return _getAccountOwner(account) != address(0);
 	}
 
@@ -109,7 +114,7 @@ contract K1Validator is IK1Validator, HybridValidatorBase, ERC1271 {
 		return _validateSignatureForOwner(_getAccountOwner(msg.sender), hash, signature);
 	}
 
-	function _erc1271CallerIsSafe(address sender) internal view virtual override returns (bool flag) {
+	function _erc1271CallerIsSafe(address sender) internal view virtual override returns (bool) {
 		return super._erc1271CallerIsSafe(sender) || _safeSenders.contains(msg.sender, sender);
 	}
 
@@ -142,7 +147,7 @@ contract K1Validator is IK1Validator, HybridValidatorBase, ERC1271 {
 	function _setAccountOwner(address owner) internal virtual {
 		assembly ("memory-safe") {
 			mstore(0x00, shr(0x60, shl(0x60, caller())))
-			mstore(0x20, _accountOwners.slot)
+			mstore(0x20, ACCOUNT_OWNERS_SLOT)
 			sstore(keccak256(0x00, 0x40), owner)
 		}
 	}
@@ -150,7 +155,7 @@ contract K1Validator is IK1Validator, HybridValidatorBase, ERC1271 {
 	function _getAccountOwner(address account) internal view virtual returns (address owner) {
 		assembly ("memory-safe") {
 			mstore(0x00, shr(0x60, shl(0x60, account)))
-			mstore(0x20, _accountOwners.slot)
+			mstore(0x20, ACCOUNT_OWNERS_SLOT)
 			owner := sload(keccak256(0x00, 0x40))
 		}
 	}
