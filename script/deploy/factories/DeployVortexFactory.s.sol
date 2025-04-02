8d69f4598691b5ccdfd6c9636177fc3b017d6624
Ryan <rkim@greenwood.app>
2025-04-02 13:41:45 -0700

    added deploy scripts

---
+++ script/deploy/factories/DeployVortexFactory.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {stdJson} from "forge-std/StdJson.sol";
+import {BaseScript} from "script/deploy/BaseScript.s.sol";
+import {Deploy, K1ValidatorFactory} from "test/shared/utils/Deploy.sol";
+
+contract DeployK1ValidatorFactory is BaseScript {
+	using stdJson for string;
+
+	function run() public returns (K1ValidatorFactory factory) {
+		bytes32 salt = getContractSalt("K1ValidatorFactory");
+		factory = Deploy.k1ValidatorFactory(
+			salt,
+			getAddress("Vortex"),
+			getAddress("Bootstrap"),
+			getAddress("K1Validator")
+		);
+
+		string memory output = "deployment";
+		output = constructJson(address(factory), broadcaster, salt);
+		output.write(getDeploymentsPath(chainAlias()), ".deployments.K1ValidatorFactory");
+
+		getMetaFactory().authorize(address(factory));
+	}
+}
